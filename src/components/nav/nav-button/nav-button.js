cc5e6d659a44c72f6f7b229ef8f4818ce0f61374
Anton Oshurek <oshurekdev@proton.me>
2024-06-19 20:07:02 +0200

    mobile menu refactoring and add esc close menu

--- src/components/nav/nav-button/nav-button.js
+++ src/components/nav/nav-button/nav-button.js
@@ -3,10 +3,30 @@ const mobileMenu = () => {
 	const mobileBtnIcon = document.querySelector('.nav-button__burger');
 	const navigation = document.querySelector('.nav');
 	const body = document.querySelector('.body');
-	let menuStatus;
 
-	const toogleMobileMenu = () => {
-		menuStatus ? closeMobileMenu() : openMobileMenu();
+	const MENU_STATUSES = {
+		OPEN: 'open',
+		CLOSE: 'close',
+	};
+
+	let menuStatus = MENU_STATUSES.CLOSE;
+
+	const menuSwitcher = () => {
+		switch (menuStatus) {
+			case MENU_STATUSES.OPEN:
+				closeMobileMenu();
+				break;
+			case MENU_STATUSES.CLOSE:
+				openMobileMenu();
+				break;
+		}
+	};
+
+	const onEscKeydown = (evt) => {
+		if (evt.key === 'Escape') {
+			evt.preventDefault();
+			closeMobileMenu();
+		}
 	};
 
 	const onBackgroundClick = (evt) => {
@@ -19,21 +39,23 @@ const mobileMenu = () => {
 		navigation.classList.add('nav--open');
 		body.classList.add('body--nav-open');
 		mobileBtnIcon.classList.add('nav-button__burger--active');
-		menuStatus = true;
+		menuStatus = MENU_STATUSES.OPEN;
 
 		body.addEventListener('click', onBackgroundClick);
+		document.addEventListener('keydown', onEscKeydown);
 	}
 
 	function closeMobileMenu() {
 		navigation.classList.remove('nav--open');
 		body.classList.remove('body--nav-open');
 		mobileBtnIcon.classList.remove('nav-button__burger--active');
-		menuStatus = false;
+		menuStatus = MENU_STATUSES.CLOSE;
 
 		body.removeEventListener('click', onBackgroundClick);
+		document.removeEventListener('keydown', onEscKeydown);
 	}
 
-	mobileButton.addEventListener('click', toogleMobileMenu);
+	mobileButton.addEventListener('click', menuSwitcher);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
