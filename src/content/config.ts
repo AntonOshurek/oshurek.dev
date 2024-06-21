c9b566073a9339aa764a52836f7d34260e43954f
Anton Oshurek <oshurekdev@proton.me>
2024-06-21 19:32:49 +0200

    add new test article and create define collection object

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const articlesCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		date: z.string(),
+		tags: z.string(),
+		overview: z.string(),
+		previewImage: z.string(),
+		previewImageAlt: z.string(),
+	}),
+});
+
+export const collections = {
+	articles: articlesCollection,
+};
