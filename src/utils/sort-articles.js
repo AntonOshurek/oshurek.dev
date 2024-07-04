147186f426ed85519bed868fe6d12b9acda869a0
Anton Oshurek <oshurekdev@proton.me>
2024-07-04 20:24:20 +0200

    add sort articles by date utilit

---
+++ src/utils/sort-articles.js
@@ -0,0 +1,7 @@
+export const sortArticles = (articles) => {
+	return articles.sort((a, b) => {
+		const da = new Date(a.data.date);
+		const db = new Date(b.data.date);
+		return db.getTime() - da.getTime();
+	});
+};
