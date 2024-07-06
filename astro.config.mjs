a7f96f7141ddcf69cd250a8ec3038f95f86a167b
Anton Oshurek <oshurekdev@proton.me>
2024-07-06 13:14:41 +0200

    add not found page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://antonoshurek.github.io',
-	base: '/oshurek.dev',
+	// site: 'https://antonoshurek.github.io',
+	// base: '/oshurek.dev',
 	integrations: [mdx(), sitemap()],
 });
