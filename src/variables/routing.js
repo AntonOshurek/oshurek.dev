edcaaddd640e6c11991a2d49de7f627c1385305c
Anton Oshurek <oshurekdev@proton.me>
2024-07-06 13:29:35 +0200

    add routing variables

---
+++ src/variables/routing.js
@@ -0,0 +1,14 @@
+export const ROUTING = {
+	ROOT: {
+		path: '/',
+		route: '',
+	},
+	PROJECTS: {
+		path: '/projects',
+		route: 'projects',
+	},
+	ARTICLES: {
+		path: '/articles',
+		route: 'articles',
+	},
+};
