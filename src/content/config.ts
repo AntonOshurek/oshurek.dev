e9fc484fadfd16770c0b2d4fc44a98cee3789035
Anton Oshurek <oshurekdev@proton.me>
2024-07-07 21:07:17 +0200

    add projects content

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,19 @@ const articlesCollection = defineCollection({
 	}),
 });
 
+const projectsCollection = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		date: z.string(),
+		tags: z.string(),
+		overview: z.string(),
+		previewImage: z.string(),
+		previewImageAlt: z.string(),
+		isFeatured: z.boolean(),
+	}),
+});
+
 export const collections = {
 	articles: articlesCollection,
 };
