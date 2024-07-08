70c5f471104e93003e6aeff21299671e462beb44
Anton Oshurek <oshurekdev@proton.me>
2024-07-08 21:03:26 +0200

    fix url params and routing for gh pages

--- src/variables/routing.js
+++ src/variables/routing.js
@@ -1,20 +1,19 @@
+//base url for deploy to github
 export const BASE_URL = '/oshurek.dev';
+//base url to deploy to hosting
+// export const BASE_URL = '';
 
 export const ROUTING = {
 	ROOT: {
 		path: `${BASE_URL}/`,
-		route: '',
 	},
 	PROJECTS: {
 		path: `${BASE_URL}/projects`,
-		route: 'projects',
 	},
 	ARTICLES: {
-		path: `${BASE_URL}articles`,
-		route: 'articles',
+		path: `${BASE_URL}/articles`,
 	},
 	ARTICLE: {
-		path: `${BASE_URL}article`,
-		route: 'article',
+		path: `${BASE_URL}/article`,
 	},
 };
