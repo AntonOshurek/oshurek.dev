743b925aeb033e6fcf479d4bf9a6b3e098880d1d
Anton Oshurek <oshurekdev@proton.me>
2024-11-22 22:07:47 +0100

    update astro v

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oshurek.dev',
-  base: '/',
-  integrations: [mdx(), sitemap()]
+	site: 'https://oshurek.dev',
+	base: '/',
+	integrations: [mdx(), sitemap()],
 });
